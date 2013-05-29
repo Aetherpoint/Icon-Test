f4f63f4b9c63e8c31cefcad694ad701d1a850109
Aetherpoint <inharmony88@gmail.com>
2013-05-29 16:50:42 -0400

    adds grunticon

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+	grunt.loadNpmTasks('grunt-grunticon');
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+	grunticon: {
+		myIcons: {
+			options: {
+			src: "css/dist/icons/",
+			dest: "css/icons/"
+			}
+		}
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+  // Default task(s).
+  grunt.registerTask('icon', ['lint qunit concat min grunticon']);
+
+};
